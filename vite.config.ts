96926c2aed0cddd796345433a4aec62352ec0761
Mathis Neunzig <mathis.neunzig@gmail.com>
2025-08-23 17:48:36 +0200

    Update project configuration: add .gitignore, modify package.json dependencies, enhance tsconfig, and implement Vite proxy settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const graphqlTarget = env.VITE_PROXY_TARGET
+  const graphqlUpstreamPath = env.VITE_GRAPHQL_PATH || '/graphql'
+
+  const tokenProxyPath = env.VITE_AAD_TOKEN_PROXY || '/aad/token'
+  const tokenUrlStr = env.VITE_AAD_TOKEN_URL || ''
+  const tokenUrl = new URL(tokenUrlStr)
+  const tokenTarget = `${tokenUrl.protocol}//${tokenUrl.host}`
+  const tokenPathname = tokenUrl.pathname
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/graphql': {
+          target: graphqlTarget || 'http://localhost:9999',
+          changeOrigin: true,
+          secure: true,
+          rewrite: () => graphqlUpstreamPath,
+          headers: { accept: 'application/json' },
+          configure: (proxy) => {
+            proxy.on('proxyReq', (req) => {
+              req.setHeader('accept', 'application/json')
+              req.setHeader('content-type', 'application/json')
+              req.setHeader('X-Requested-With', 'XMLHttpRequest')
+            })
+          },
+        },
+        [tokenProxyPath]: {
+          target: tokenTarget,
+          changeOrigin: true,
+          secure: true,
+          rewrite: () => tokenPathname,
+          headers: { accept: 'application/json' },
+          configure: (proxy) => {
+            proxy.on('proxyReq', (req) => {
+              if ('origin' in req.getHeaders()) req.removeHeader('origin')
+              req.setHeader('accept', 'application/json')
+              req.setHeader('content-type', 'application/x-www-form-urlencoded')
+            })
+          },
+        },
+      },
+    },
+  }
 })
